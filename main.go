1b4b1897628b1ba0deaa047c847f2572c1ccf544
Simon Delicata <simon.delicata@free.fr>
2021-08-30 14:45:15 +0200

    feat: add a simple reverse proxy with httputil

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/sdelicata/caeche/server"
+	log "github.com/sirupsen/logrus"
+	"os"
+)
+
+func init() {
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+}
+
+func main() {
+	server := server.NewServer()
+	server.Start()
+}
---
+++ server/server.go
@@ -0,0 +1,46 @@
+package server
+
+import (
+	"crypto/tls"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"time"
+)
+
+type Server struct {
+	proxy *httputil.ReverseProxy
+	httpServer *http.Server
+}
+
+func (server *Server) Start() {
+	log.Debugf("Server starting...")
+	server.httpServer.ListenAndServe()
+}
+
+func NewServer() *Server {
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
+		Scheme: "http",
+		Host:   fmt.Sprintf("google.com"),
+	})
+
+	httpServer := &http.Server{
+		Addr:         ":8080",
+		Handler:      proxy,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+	}
+
+	server := &Server{
+		proxy: proxy,
+		httpServer: httpServer,
+	}
+
+	return server
+}
+
+
